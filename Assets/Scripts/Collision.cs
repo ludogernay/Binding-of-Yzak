5bce0497754cfba87c563d191fd5567b2f893a8d
Hallexxx <112977841+Hallexxx@users.noreply.github.com>
2023-03-23 16:25:24 +0100

    v5

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -6,13 +6,36 @@ using UnityEngine.SceneManagement;
 public class Collision : MonoBehaviour
 {
     // Start is called before the first frame update
+    public So so;
     public GameObject Object;
+    private SpriteRenderer spriteRenderer;
+
+   
+    void Start() 
+    { 
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
     public void OnCollisionEnter2D()
     {
-        Object.SetActive(true);
+        if (so.win)
+        {
+            Object.SetActive(false);
+        }
+        else
+        {
+            Object.SetActive(true);
+        }
     }
     public void OnCollisionExit2D()
     {
         Object.SetActive(false);
+    } 
+    
+    public void Update() {
+        if (so.win)
+        {
+            spriteRenderer.color = new Color (1, 0, 0, 1); 
+        }
     }
 }
